112e0eb81052369c1c47c71e62440bb8fb5e4d32
Omkar <omkarpopalghat1729@gmail.com>
2021-04-10 18:29:07 +0530

    Minor changes

--- step_lap/fishy_fish.py
+++ step_lap/fishy_fish.py
@@ -7,6 +7,15 @@ Created on Thu Apr  8 23:09:40 2021
 """
 
 import pandas as pd 
+import os
+
+class PandasModule():
+    def __init__(self):
+        self.file_name = ''
+        
+    def checkFileName(self, name):
+        pass
+            
     
 class JobProfile():
     def __init__(self):
@@ -17,14 +26,7 @@ class JobProfile():
         self.tool_list = []
         self.length_list = []
         self.fish_len = 0
-        
-    def getToolList(self):
-        while True:
-            cmd = input('Add Tool ? - ').lower()
-            if cmd in ['y', 'a']:
-                self.tool_list.append(input('Enter name : '))
-            else:
-                break
+        self.hole = []
             
     
     def getStepLapInfo(self):
@@ -35,11 +37,24 @@ class JobProfile():
     def getLengthList(self):
         self.length_list = [int(i) for i in input('Enter lengths : ').split()]
         self.fish_len = sum(self.length_list)
+        self.createHoleList()
+        
+    def createHoleList(self):
+        pos = 0
+        ret = []
+        if len(self.length_list) > 1:
+            for i in range(len(self.length_list)-1):
+                pos = pos + self.length_list[i]
+                ret.append(pos)
+        self.hole = ret
     
     def createDict(self):
         exe = []
         self.pattern_length = self.step_lap_count*(self.fish_len + (self.step_lap_count - 1)*self.step_lap_distance )
         for i in range(self.step_lap_count):
+            if len(self.hole) > 0:
+                for j in self.hole:
+                    exe.append(['h', j + (2*self.k - i + 2*self.k*i)*self.step_lap_distance + i*self.fish_len])
             exe.append(['fm45', i*self.fish_len + (3+i)*self.k*self.step_lap_distance])
             exe.append(['fp45',(i+1)*self.fish_len + (3+i)*self.k*self.step_lap_distance])
             exe.append(['v', i*(self.fish_len + (self.step_lap_count - 1) * self.step_lap_distance)])
@@ -49,6 +64,8 @@ class JobProfile():
         for i in self.exe:
             if i[0][0] == 'f':
                 i[1] += 4335
+            elif i[0] == 'h':
+                i[1] += 1250
         terminate = 100
         feed = []
         vaxis = []
@@ -79,10 +96,11 @@ class JobProfile():
         temp = pd.ExcelWriter('../cut_program_output/FishyFish_0.xlsx')
         df.to_excel(temp)
         temp.save()
-            
         
 jp = JobProfile()
 jp.getStepLapInfo()
 jp.getLengthList()
 jp.createDict()
+# for i in sorted(jp.exe, key = lambda x: x[1]):
+#     print(i[0], '--', i[1])
 jp.execute()
