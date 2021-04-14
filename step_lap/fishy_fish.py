a07025f05794cd3aafcdfa09d7023f4f716f01b9
Omkar <omkarpopalghat1729@gmail.com>
2021-04-14 20:05:58 +0530

    Corrected the generalized formula

--- step_lap/fishy_fish.py
+++ step_lap/fishy_fish.py
@@ -78,8 +78,8 @@ class JobProfile():
                     exe.append(['h', i*(l+2*k*d) + j + (2*k - i//m)*d])
             #exe.append(['fm45', i*self.fish_len + (3+(i//m))*self.k*self.step_lap_distance])
             #exe.append(['fp45',((i//m) + 1)*self.fish_len + (3+(i//m))*self.k*self.step_lap_distance])
-            exe.append(['fm45', (3-i//m)*k*d + (l+(n-1)*d)*i])
-            exe.append(['fp45', (1-i//m)*k*d + ((l+(n-1)*d)*(i+1))])
+            exe.append(['fm45', (3*k-2*(i//m))*d + (l+(n-1)*d)*i])
+            exe.append(['fp45', (k-2*(i//m))*d + ((l+(n-1)*d)*(i+1))])
             exe.append(['v', i*(self.fish_len + (self.step_lap_count - 1) * self.step_lap_distance)])
         self.exe = exe
         self.pattern_length *= m
