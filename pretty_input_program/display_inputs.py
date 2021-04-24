a113ae09fa417664946df87603b0a6129100b46b
Omkar <omkarpopalghat1729@gmail.com>
2021-04-24 16:39:59 +0530

    Added display for split-yoke and central-limb

--- pretty_input_program/display_inputs.py
+++ pretty_input_program/display_inputs.py
@@ -14,12 +14,13 @@ from os import listdir
 
 class CutProgram:
     
-    def __init__(self, name):
+    def __init__(self, name, path):
+        self.path = path
         self.data = self.getData(name)
         
     def getData(self, name):
         data = {}
-        with open('../cut_program_input/'+name) as file:
+        with open(self.path+name) as file:
             data = json.load(file)
         if data:
             return data
@@ -27,65 +28,123 @@ class CutProgram:
         sys.exit()
         
 
-def listCutPrograms():
-    names = [i for i in listdir('../cut_program_input/') if i.endswith('json')]
+def listCutPrograms(path):
+    names = [i for i in listdir(path) if i.endswith('json')]
     for i in range(len(names)):
         print(f'{i+1} - {names[i]}')
     return names
 
+
+def plotYokeSplitter(pos):
+    y1 = np.linspace(-1, 1, 10)
+    y2 = np.linspace(0, 1, 5)
+    plt.plot(pos+y2, y2, color='black')
+    plt.plot(pos-y1, y1, color='black')
+
+def plotSpear(is_front, pos, h=0,linestyle = None):
+    y1 = np.linspace(h, 1, 5)
+    y2 = np.linspace(-1, h, 5)
+    if not is_front:
+        if linestyle:
+            plt.plot(pos-y1+h, y1, color='cyan', linestyle=linestyle)
+            plt.plot(pos+y2-h, y2, color='cyan', linewidth=linestyle)
+        else:
+            if pos == int(pos):
+                plt.plot(pos-y1+h, y1, color='black')
+                plt.plot(pos+y2-h, y2, color='black')
+            else:
+                plt.plot(pos-y1+h, y1, color='cyan')
+                plt.plot(pos+y2-h, y2, color='cyan')
+    else:
+        if linestyle:
+            plt.plot(pos + y1 - h, y1, linestyle=linestyle)
+            plt.plot(pos - y2 + h, y2, linestyle = linestyle)
+        else :
+            if pos == int(pos):
+                plt.plot(pos + y1 - h, y1, color='black')
+                plt.plot(pos - y2 + h, y2, color='black')
+            else:
+                plt.plot(pos + y1 - h, y1, color='cyan')
+                plt.plot(pos - y2 + h, y2, color='cyan')
+            
+
+            
+
 def plotFm45(pos, linestyle=None):
     y = np.linspace(-1, 1, 10)
     if linestyle:
-        plt.plot(y+pos, y, color='blue', linestyle=linestyle)
+        plt.plot(y+pos, y, color='cyan', linestyle=linestyle)
     else:
-        plt.plot(y+pos, y, color='blue')
+        if pos - int(pos) == 0:
+            plt.plot(y+pos, y, color='black')
+        else:
+            plt.plot(y+pos, y, color='cyan')
     
 def plotFp45(pos, linestyle=None):
     y = np.linspace(-1, 1, 10)
     if linestyle:
-        plt.plot(-y+pos, y, color='blue', linestyle=linestyle)
+        plt.plot(-y+pos, y, color='cyan', linestyle=linestyle)
     else:
-        plt.plot(-y+pos, y, color='blue')
-    
+        if pos - int(pos) == 0:
+            plt.plot(-y+pos, y, color='black')
+        else:
+            plt.plot(-y+pos, y, color='cyan')
+            
 def plotF0(pos, linestyle=None):
     y = np.linspace(-1, 1, 10)
     if linestyle:
-        plt.plot(pos, y, color='blue', linestyle=linestyle)
+        plt.plot([pos]*10, y, color='cyan', linestyle=linestyle)
     else:
-        plt.plot(pos, y, color='blue')
+        if pos - int(pos) == 0:
+            plt.plot([pos]*10, y, color='black')
+        else:
+            plt.plot([pos]*10, y, color='cyan')
     
-def plotVnotch(pos):
-    y = np.linspace(0,1, 5)
-    plt.plot(-y+pos, y, color = 'blue', linewidth=1)
-    plt.plot(y+pos, y, color = 'blue', linewidth=1)
+def plotVnotch(pos,ls=0, linestyle=None):
+    y = np.linspace(0 + ls,1, 5)
+    if ls != 0:
+        if linestyle:
+            
+            plt.plot(-y+pos, y, color = 'cyan', linestyle='dotted')
+            plt.plot(y+pos, y, color = 'cyan', linestyle='dotted')
+        else:
+            plt.plot(-y+pos, y, color = 'cyan', linewidth=1)
+            plt.plot(y+pos, y, color = 'cyan', linewidth=1)
+            
+    else:
+        plt.plot(-y+pos, y, color = 'black', linewidth=1)
+        plt.plot(y+pos, y, color = 'black', linewidth=1)
     
 def plotHole(pos, ax):
-    ax.add_patch(plt.Circle((pos,0), 0.2,color='blue', fill=False))
+    ax.add_patch(plt.Circle((pos,0), 0.2,color='black', fill=False))
     
 def annotate(a, b, count):
     if count > 0:
         plt.annotate('', xy=(b,-2), xytext=(a,-2), arrowprops=dict(arrowstyle='<->'))
         plt.annotate('L'+str(count),xy=(b,-2.5), xytext=((a+b)/2 - 0.15,-2.5) )
         
-def main():
+def main(path=None):
     while 1:
-        names = listCutPrograms()
+        names = listCutPrograms(path)
         file_index = input('\n Enter index (q to exit): ')
         if file_index == 'q':
             print('Bye ...')
-            sys.exit()
+            return
+            #sys.exit()
         file_index = int(file_index)
         x = [i for i in range(100)]
         top = [1]*100
         bot = [-1]*100
         mid = [0]*100
-        fig, ax = plt.subplots()
-        cp = CutProgram(names[file_index-1])
+        cp = CutProgram(names[file_index-1], path)
         data = cp.data
+        
+        width = len(data.keys())
+        fig, ax = plt.subplots(figsize=(width+3, 5))
         pos, prev = 0, 0
         for i in range(len(data.keys())):
             prev = pos
-            pos = 3*i + 1
+            pos = 3*i + 2
             curr_tool = data[str(i)]
             if curr_tool['name'] == 'fm45':
                 if curr_tool['step_lap_count'] > 1:
@@ -151,6 +210,14 @@ def main():
                 
                 
             elif curr_tool['name'] == 'v':
+                if curr_tool['lateral_shift_count'] > 1:
+                    if curr_tool['is_open']:
+                        plotVnotch(pos, 0.1)
+                        plotVnotch(pos, 0.2)
+                    else:
+                        plotVnotch(pos, 0.1, 'dotted')
+                        plotVnotch(pos, 0.2, 'dotted')
+                        
                 plotVnotch(pos)
                 
                 
@@ -193,10 +260,132 @@ def main():
         plt.plot(x, top , color = 'black')
         plt.plot( x, bot, color = 'black')
         plt.plot(x, mid , 'b-.', linewidth=1)
-        plt.xlim(0, pos+1)
+        
+        plt.annotate('', xy=(-1,0), xytext=(-2,0), arrowprops=dict(arrowstyle='<-'))
+        
+        plt.xlim(-3, pos+3)
         plt.ylim(-4,4)
+        plt.axis('off')
         plt.show()
+    return
+        
+        
+def main2(path):
+    while 1:
+        names = listCutPrograms(path)
+        file_index = input('\n Enter index (q to exit): ')
+        if file_index == 'q':
+            print('Bye ...')
+            return
+            #sys.exit()
+        file_index = int(file_index)
+        x = [i for i in range(100)]
+        top = [1]*100
+        bot = [-1]*100
+        mid = [0]*100
+        cp = CutProgram(names[file_index-1], path)
+        data = cp.data
+        
+        width = len(data.keys())
+        fig, ax = plt.subplots(figsize=(width+3, 5))
+        pos, prev, front_c = 0, 0, 0
+        for i in range(len(data.keys())):
+            prev = pos
+            pos = 3*i + 2
+            ct = data[str(i)]
+            if ct['name'] == 'fm45':
+                plotFm45(pos)
+                if ct['slp_count'] > 1:
+                    if ct['open']:
+                        if front_c == 0:
+                            front_c+=1
+                            plotFm45( pos - 0.2 )
+                            plotFm45(pos - 0.4)
+                        else :
+                            plotFm45( pos + 0.2 )
+                            plotFm45(pos + 0.4)
+                    else:
+                        if front_c == 0:
+                            front_c+=1
+                            plotFm45(pos + 0.2, 'dotted')
+                            plotFm45(pos + 0.4, 'dotted')
+                        else:
+                            plotFm45(pos - 0.2, 'dotted')
+                            plotFm45(pos - 0.4, 'dotted')
+                            
+            elif ct['name'] == 'fp45':
+                plotFp45(pos)
+                if ct['slp_count'] > 1:
+                    if ct['open']:
+                        if front_c == 0:
+                            plotFp45( pos - 0.2 )
+                            plotFp45(pos - 0.4)
+                        else :
+                            plotFp45( pos + 0.2 )
+                            plotFp45(pos + 0.4)
+                    else:
+                        if front_c == 0:
+                            plotFp45(pos + 0.2, 'dotted')
+                            plotFp45(pos + 0.4, 'dotted')
+                        else:
+                            plotFp45(pos - 0.2, 'dotted')
+                            plotFp45(pos - 0.4, 'dotted')
+                            
+            elif ct['name'] == 's':
+                if ct['step_lap_count'] > 1:
+                    if ct['is_open']:
+                        if ct['is_front']:
+                            plotSpear(True, pos)
+                            plotSpear(True, pos + 0.2)
+                            plotSpear(True, pos + 0.4)
+                        else:
+                            plotSpear(False, pos)
+                            plotSpear(False, pos - 0.2)
+                            plotSpear(False, pos - 0.4)
+                    else:
+                        if ct['is_front']:
+                            plotSpear(True, pos)
+                            plotSpear(True, pos + 0.2, linestyle='dotted')
+                            plotSpear(True, pos + 0.4, linestyle='dotted')
+                        else:
+                            plotSpear(False, pos)
+                            plotSpear(False, pos - 0.2, linestyle='dotted')
+                            plotSpear(False, pos - 0.4, linestyle='dotted')
+                            
+            elif ct['name'] == 'ys':
+                plotYokeSplitter(pos)
+                
+            elif ct['name'] == 'h':
+                plotHole(pos, ax)
+            annotate(prev, pos, i)
+        plt.plot(x, top , color = 'black')
+        plt.plot( x, bot, color = 'black')
+        plt.plot(x, mid , 'b-.', linewidth=1)
+        
+        plt.annotate('', xy=(-1,0), xytext=(-2,0), arrowprops=dict(arrowstyle='<-'))
+        
+        plt.xlim(-3, pos+3)
+        plt.ylim(-4,4)
+        plt.axis('off')
+        plt.show()
+    return
 
+                    
                 
 if __name__ == "__main__":
-    main()
+    while 1:
+        print('1 - Simple step-lap')
+        print('2 - Central Limb')
+        print('3 - Split yoke')
+        opt = input('Enter option : ')
+        if opt == '1':
+            
+            main('../cut_program_input/')
+            
+        elif opt == '2':
+            main2('../cut_program_input/central_limb/')
+            
+        elif opt == '3':
+            main2('../cut_program_input/split_yoke/')
+        elif opt == 'q':
+            break
