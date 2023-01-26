a4211792a6fb3c8f9b615dbe66156128fd4083b3
Omkar <omkarpopalghat1729@gmail.com>
2023-01-26 11:04:01 +0530

    skewed fishy correction

--- step_lap/fishy_fish.py
+++ step_lap/fishy_fish.py
@@ -96,7 +96,7 @@ class JobProfile():
                         exe.append(['h', i*(l+mult*(n - 1)*d) + j + (2*k - i//m)*d])
                 #exe.append(['fm45', i*self.fish_len + (3+(i//m))*self.k*self.step_lap_distance])
                 #exe.append(['fp45',((i//m) + 1)*self.fish_len + (3+(i//m))*self.k*self.step_lap_distance])
-                exe.append(['fm45', (3*k-2*(i//m))*d + (l+mult*k*d)*i])
+                exe.append(['fm45', (3*k-2*(i//m))*d + (l+mult*(n-1)*d)*i])
                 exe.append(['fp45', (k-2*(i//m))*d + ((l+mult*(n-1)*d)*(i+1))])
                 exe.append(['v', i*(self.fish_len + mult * (self.step_lap_count - 1) * self.step_lap_distance)])
             self.exe = exe
@@ -112,7 +112,7 @@ class JobProfile():
                 i[1] += 1250.125
             i[1] = round(i[1], 5)
 
-        terminate = 500
+        terminate = 200
         feed = []
         vaxis = []
         operation = []
